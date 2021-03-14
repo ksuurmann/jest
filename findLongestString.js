d9230438de41b280c929e317b44b780b2621fe0f
ksuurmann <71810372+ksuurmann@users.noreply.github.com>
2021-03-14 19:48:35 +0200

    updated verison of faulty code

--- findLongestString.js
+++ findLongestString.js
@@ -1,10 +1,10 @@
 const findLongest = (arr) => {
   let longest = '';
     for (let i = 0; i < arr.length; i++) {
-      if (arr[i] != "string") {
+      if (typeof arr[i] != "string") {
         return "only strings are allowed";
       } else {
-        if (arr[i].length < longest.length) {
+        if (arr[i].length > longest.length) {
             longest = arr[i];
         }
       }
